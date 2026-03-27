c148341f2579415fc31c71efb771cc0928accc88
crash0verride11 <3526616+crash0verride11@users.noreply.github.com>
2026-03-27 18:30:00 -0400

    fix conflicts

--- tests/test_connection_management.py
+++ tests/test_connection_management.py
@@ -173,7 +173,6 @@ async def test_wakeup_called_after_pair(mock_idasen_desk: MagicMock):
     [
         (Exception(), Exception),
         (BleakDBusError("org.bluez.Error.AuthenticationFailed", ""), AuthFailedError),
-        (BleakDBusError("org.bluez.Error.Failed", ""), BleakDBusError),
     ],
 )
 async def test_disconnect_on_pair_failure(
@@ -187,29 +186,6 @@ async def test_disconnect_on_pair_failure(
     with pytest.raises(raised_exception):
         await desk.connect(FAKE_BLE_DEVICE, retry=False)
     assert not desk.is_connected
-    mock_idasen_desk.disconnect.assert_called()
-    mock_idasen_desk.wakeup.assert_not_called()
-    assert update_callback.call_count == 0
-
-
-async def test_wakeup_happens_after_pair(mock_idasen_desk: MagicMock):
-    """Test that wakeup is awaited after pairing succeeds."""
-    desk = Desk(Mock(), False)
-    await desk.connect(FAKE_BLE_DEVICE)
-    mock_idasen_desk.assert_has_calls([call.pair(), call.wakeup()])
-
-
-async def test_disconnect_on_wakeup_failure(mock_idasen_desk: MagicMock):
-    """Test that disconnect is called if wakeup fails."""
-    update_callback = Mock()
-    desk = Desk(update_callback, False)
-
-    mock_idasen_desk.wakeup.side_effect = BleakError()
-
-    with pytest.raises(BleakError):
-        await desk.connect(FAKE_BLE_DEVICE, retry=False)
-
-    mock_idasen_desk.pair.assert_called_once()
     mock_idasen_desk.disconnect.assert_called()
     assert update_callback.call_count == 0
 
